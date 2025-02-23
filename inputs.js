6162b0ea8aed9e9d2df0a4cb3b15d29ce516f43f
JoaoPMV <joaop.trilha@gmail.com>
2025-02-23 00:12:40 -0300

    third  commit

--- inputs.js
+++ inputs.js
@@ -6,9 +6,20 @@ const writers = document.getElementById("writers");
 const sightseeing = document.getElementById("sightseeing");
 const punctual = document.getElementById("punctual");
 const coming = document.getElementById("coming");
+const great = document.getElementById("great");
+const really = document.getElementById("really");
+const view = document.getElementById("view");
+const introduce = document.getElementById("introduce");
+const start = document.getElementById("start");
+const ideas = document.getElementById("ideas");
+const tired = document.getElementById("tired");
+const finish = document.getElementById("finish");
+const that = document.getElementById("that");
+const just = document.getElementById("just");
+const again = document.getElementById("again");
 
 myName.addEventListener("input", function () {
-  if (this.value === "name") {
+  if (this.value.toLowerCase() === "name") {
     this.style.border = "2px solid green";
     this.style.backgroundColor = "#ADFF2F";
     this.style.color = "green";
@@ -19,7 +30,7 @@ myName.addEventListener("input", function () {
 });
 
 months.addEventListener("input", function () {
-  if (this.value === "months") {
+  if (this.value.toLowerCase() === "months") {
     this.style.border = "2px solid green";
     this.style.backgroundColor = "#ADFF2F";
     this.style.color = "green";
@@ -30,7 +41,7 @@ months.addEventListener("input", function () {
 });
 
 visited.addEventListener("input", function () {
-  if (this.value === "visited") {
+  if (this.value.toLowerCase() === "visited") {
     this.style.border = "2px solid green";
     this.style.backgroundColor = "#ADFF2F";
     this.style.color = "green";
@@ -41,7 +52,7 @@ visited.addEventListener("input", function () {
 });
 
 meetings.addEventListener("input", function () {
-  if (this.value === "meetings") {
+  if (this.value.toLowerCase() === "meetings") {
     this.style.border = "2px solid green";
     this.style.backgroundColor = "#ADFF2F";
     this.style.color = "green";
@@ -52,7 +63,7 @@ meetings.addEventListener("input", function () {
 });
 
 writers.addEventListener("input", function () {
-  if (this.value === "writers") {
+  if (this.value.toLowerCase() === "writers") {
     this.style.border = "2px solid green";
     this.style.backgroundColor = "#ADFF2F";
     this.style.color = "green";
@@ -63,7 +74,7 @@ writers.addEventListener("input", function () {
 });
 
 sightseeing.addEventListener("input", function () {
-  if (this.value === "sightseeing") {
+  if (this.value.toLowerCase() === "sightseeing") {
     this.style.border = "2px solid green";
     this.style.backgroundColor = "#ADFF2F";
     this.style.color = "green";
@@ -74,7 +85,7 @@ sightseeing.addEventListener("input", function () {
 });
 
 punctual.addEventListener("input", function () {
-  if (this.value === "punctual") {
+  if (this.value.toLowerCase() === "punctual") {
     this.style.border = "2px solid green";
     this.style.backgroundColor = "#ADFF2F";
     this.style.color = "green";
@@ -85,7 +96,128 @@ punctual.addEventListener("input", function () {
 });
 
 coming.addEventListener("input", function () {
-  if (this.value === "coming") {
+  if (this.value.toLowerCase() === "coming") {
+    this.style.border = "2px solid green";
+    this.style.backgroundColor = "#ADFF2F";
+    this.style.color = "green";
+  } else {
+    this.style.border = "2px solid red";
+    this.style.color = "red";
+  }
+});
+
+great.addEventListener("input", function () {
+  if (this.value.toLowerCase() === "great") {
+    this.style.border = "2px solid green";
+    this.style.backgroundColor = "#ADFF2F";
+    this.style.color = "green";
+  } else {
+    this.style.border = "2px solid red";
+    this.style.color = "red";
+  }
+});
+
+really.addEventListener("input", function () {
+  if (this.value.toLowerCase() === "really") {
+    this.style.border = "2px solid green";
+    this.style.backgroundColor = "#ADFF2F";
+    this.style.color = "green";
+  } else {
+    this.style.border = "2px solid red";
+    this.style.color = "red";
+  }
+});
+
+view.addEventListener("input", function () {
+  if (this.value.toLowerCase() === "view") {
+    this.style.border = "2px solid green";
+    this.style.backgroundColor = "#ADFF2F";
+    this.style.color = "green";
+  } else {
+    this.style.border = "2px solid red";
+    this.style.color = "red";
+  }
+});
+
+introduce.addEventListener("input", function () {
+  if (this.value.toLowerCase() === "introduce") {
+    this.style.border = "2px solid green";
+    this.style.backgroundColor = "#ADFF2F";
+    this.style.color = "green";
+  } else {
+    this.style.border = "2px solid red";
+    this.style.color = "red";
+  }
+});
+
+start.addEventListener("input", function () {
+  if (this.value.toLowerCase() === "start") {
+    this.style.border = "2px solid green";
+    this.style.backgroundColor = "#ADFF2F";
+    this.style.color = "green";
+  } else {
+    this.style.border = "2px solid red";
+    this.style.color = "red";
+  }
+});
+
+ideas.addEventListener("input", function () {
+  if (this.value.toLowerCase() === "ideas") {
+    this.style.border = "2px solid green";
+    this.style.backgroundColor = "#ADFF2F";
+    this.style.color = "green";
+  } else {
+    this.style.border = "2px solid red";
+    this.style.color = "red";
+  }
+});
+
+tired.addEventListener("input", function () {
+  if (this.value.toLowerCase() === "tired") {
+    this.style.border = "2px solid green";
+    this.style.backgroundColor = "#ADFF2F";
+    this.style.color = "green";
+  } else {
+    this.style.border = "2px solid red";
+    this.style.color = "red";
+  }
+});
+
+finish.addEventListener("input", function () {
+  if (this.value.toLowerCase() === "finish") {
+    this.style.border = "2px solid green";
+    this.style.backgroundColor = "#ADFF2F";
+    this.style.color = "green";
+  } else {
+    this.style.border = "2px solid red";
+    this.style.color = "red";
+  }
+});
+
+that.addEventListener("input", function () {
+  if (this.value.toLowerCase() === "that") {
+    this.style.border = "2px solid green";
+    this.style.backgroundColor = "#ADFF2F";
+    this.style.color = "green";
+  } else {
+    this.style.border = "2px solid red";
+    this.style.color = "red";
+  }
+});
+
+just.addEventListener("input", function () {
+  if (this.value.toLowerCase() === "just") {
+    this.style.border = "2px solid green";
+    this.style.backgroundColor = "#ADFF2F";
+    this.style.color = "green";
+  } else {
+    this.style.border = "2px solid red";
+    this.style.color = "red";
+  }
+});
+
+again.addEventListener("input", function () {
+  if (this.value.toLowerCase() === "again") {
     this.style.border = "2px solid green";
     this.style.backgroundColor = "#ADFF2F";
     this.style.color = "green";
